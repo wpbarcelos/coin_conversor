82b05ded05e90184e57583fc1f4dfcfa20b8d87b
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2019-12-06 17:24:56 -0300

    finalizado coin conversor

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,16 @@ import 'dart:convert';
 const request = 'https://api.hgbrasil.com/finance?format=json&key=2c2c1746';
 
 void main() async {
-  runApp(MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.amber)))),
+  ));
 }
 
 Future<Map> getData() async {
@@ -21,6 +30,50 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final realController = TextEditingController();
+  final dollarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  double dollar;
+  double euro;
+
+  void clearAll() {
+    dollarController.text = '';
+    realController.text = '';
+    euroController.text = '';
+  }
+
+  void _realChanged(String text) {
+    if (text.isEmpty) {
+      this.clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dollarController.text = (real / dollar).toStringAsFixed(2);
+    euroController.text = (real / euro).toStringAsFixed(2);
+  }
+
+  void _dollarChanged(String text) {
+    if (text.isEmpty) {
+      this.clearAll();
+      return;
+    }
+    double dollar = double.parse(text);
+    realController.text = (dollar * this.dollar).toStringAsFixed(2);
+    euroController.text = ((dollar * this.dollar) / euro).toStringAsFixed(2);
+  }
+
+  void _euroChanged(String text) {
+    if (text.isEmpty) {
+      this.clearAll();
+      return;
+    }
+    double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsFixed(2);
+    dollarController.text =
+        ((euro * this.euro) / this.dollar).toStringAsFixed(2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,10 +99,46 @@ class _HomeState extends State<Home> {
                       style: TextStyle(color: Colors.amber, fontSize: 25.0),
                       textAlign: TextAlign.center);
                 } else {
-                  return Container(color: Colors.green);
+                  dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                  return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber),
+                          Divider(),
+                          buildTextField(
+                              "Reais", "R\$ ", realController, _realChanged),
+                          Divider(),
+                          buildTextField("Dólares", "US\$ ", dollarController,
+                              _dollarChanged),
+                          Divider(),
+                          buildTextField(
+                              "Euros", "€ ", euroController, _euroChanged),
+                          Divider(),
+                        ],
+                      ));
                 }
             }
           },
         ));
   }
+}
+
+Widget buildTextField(String label, String prefix,
+    TextEditingController controller, Function fn) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+    onChanged: fn,
+    keyboardType: TextInputType.numberWithOptions(decimal: true),
+  );
 }
